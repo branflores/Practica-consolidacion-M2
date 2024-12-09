f6140683ef56ef2a7fac48db385202ce533667ef
BrandonFlores <brandon.frc@gmail.com>
2024-12-09 14:32:01 -0300

    creacion footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 let formulario = document.getElementById("form");
-
+let inputUno = document.getElementById("exampleFormControlInputUno")
+let inputDos = document.getElementById("exampleFormControlInputDos")
+let textArea = document.getElementById("exampleFormControlTextareaUno")
 
 formulario.addEventListener("submit",() => {
     alert("Tu mensaje se ha enviado con exito")
+    inputUno.value = ""
+    inputDos.value = ""
+    textArea.value = ""
 })
